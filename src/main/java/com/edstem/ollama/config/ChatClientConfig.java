42e1899c8fd3cd634b1b8a7f4d9547c04d450740
albinzdaniel02 <albinzdaniel02@gmail.com>
2026-03-16 14:50:38 +0530

    Connect h2 database

--- src/main/java/com/edstem/ollama/config/ChatClientConfig.java
+++ src/main/java/com/edstem/ollama/config/ChatClientConfig.java
@@ -1,6 +1,8 @@
 package com.edstem.ollama.config;
 
+import org.h2.server.web.JakartaWebServlet;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,5 +12,10 @@ public class ChatClientConfig {
     @Bean
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
         return chatClientBuilder.build();
+    }
+
+    @Bean
+    public ServletRegistrationBean<JakartaWebServlet> h2Console() {
+        return new ServletRegistrationBean<>(new JakartaWebServlet(), "/h2-console/*");
     }
 }
