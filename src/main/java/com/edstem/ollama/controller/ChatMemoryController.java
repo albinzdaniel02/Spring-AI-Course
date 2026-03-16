ed1c07328104478671ada51fab6350da8a6bdc9a
albinzdaniel02 <albinzdaniel02@gmail.com>
2026-03-16 13:55:28 +0530

    Add memory controller

--- src/main/java/com/edstem/ollama/controller/ChatMemoryController.java
+++ src/main/java/com/edstem/ollama/controller/ChatMemoryController.java
@@ -1,15 +1,14 @@
 package com.edstem.ollama.controller;
 
+import static org.springframework.ai.chat.memory.ChatMemory.CONVERSATION_ID;
+
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.client.advisor.api.Advisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -25,8 +24,15 @@ public class ChatMemoryController {
 
     @GetMapping("/chat-memory")
     public ResponseEntity<String> getChat(
+            @RequestHeader String username,
             @RequestParam(defaultValue = "Hello from the chat client!") String message) {
-        String response = chatClient.prompt(message).call().content();
+        String response =
+                chatClient
+                        .prompt()
+                        .user(message)
+                        .advisors(advisorSpec -> advisorSpec.param(CONVERSATION_ID, username))
+                        .call()
+                        .content();
         return ResponseEntity.ok(response);
     }
 }
